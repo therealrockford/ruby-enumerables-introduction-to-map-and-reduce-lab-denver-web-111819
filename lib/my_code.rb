372dbadb4773ba362590737d064cc7b8689fb6be
Rocky Pitto <rockypitto@gmail.com>
2019-10-23 18:20:51 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -65,11 +65,10 @@ def reduce_to_any_true(source_array)
   while i < source_array.size do
     if (source_array[i] == true)
       return true
-    else
-      return false
-    end
+    end 
     i += 1 
   end
+  return false 
 end
 
 
