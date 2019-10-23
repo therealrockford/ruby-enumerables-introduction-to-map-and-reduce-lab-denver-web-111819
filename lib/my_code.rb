ee65b6854ecca34b693549bb606a1346af07f064
Rocky Pitto <rockypitto@gmail.com>
2019-10-23 18:04:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,25 +49,21 @@ def reduce_to_total(source_array, starting_point=0)
   total  
 end
 
-def reduce_to_all_true(source_array)
-  i = 0 
-  while i < source_array.size do 
-    if (source_array[i] == 1 || source_array[i] == 2 || source_array[i] == true || source_array[i] == "razmatazz" || source_array[i] == false)
-      return true
-    end
-    i += 1 
-  end
-  return false 
-end
+# def reduce_to_all_true(source_array)
+#   i = 0 
+#   while i < source_array.size do 
+#     if (source_array[i] == 1 || source_array[i] == 2 || source_array[i] == true || source_array[i] == "razmatazz" || source_array[i] == false)
+#       return true
+#     end
+#     i += 1 
+#   end
+#   return false 
+# end
 
 def reduce_to_any_true(source_array)
   i = 0 
   while i < source_array.size do
-    if (source_array[i] == true)
-      return true
-    else
-      return false
-    end 
+    return true if (source_array[i] == true)
     i += 1 
   end
 end
