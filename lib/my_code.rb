029a2927b9695c8653bc0853626b008bb9e97149
Rocky Pitto <rockypitto@gmail.com>
2019-10-23 18:42:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,9 +52,6 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.size do 
-    # if (source_array[i] == 1 || source_array[i] == 2 || source_array[i] == true || source_array[i] == "razmatazz" || source_array[i] == false)
-    #   return true
-    # end
     if (!source_array[i])
       return false
     end 
